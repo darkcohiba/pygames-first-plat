0edfc2b1350a77418f86eb3f83f31208433220c9
Sam <samuel.waters@gmail.com>
2023-05-19 11:05:27 -0600

    chore: added images

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill("red")
+
 background_surface = pygame.image.load('graphics/bg/URy5F.png')
+background_surface = pygame.transform.scale(background_surface, (800, 400))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -19,7 +19,6 @@ while True:
     # background
     screen.blit(background_surface, (0, 0))
     # x axis, y axis, 0, 0 is the top left corner
-    screen.blit(test_surface, (100, 100))
     pygame.display.update()
     clock.tick(60)
 
