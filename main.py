63afda9e93fb274eba54e4b1e3086161bb1247b2
Sam <samuel.waters@gmail.com>
2023-05-20 16:44:27 -0600

    chore: added images

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+# set our font to default with a size of 50
+test_font = pygame.font.Font(None, 50)
+
+# test text surface for menu
+welcome_message = test_font.render("Welcome to My Game", False, "Green")
+
+
 
 
 background_surface = pygame.image.load('graphics/bg/URy5F.png')
@@ -18,6 +25,7 @@ while True:
 
     # background
     screen.blit(background_surface, (0, 0))
+    screen.blit(welcome_message, (200, 100))
     # x axis, y axis, 0, 0 is the top left corner
     pygame.display.update()
     clock.tick(60)
