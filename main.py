a4c78258c74f3f34c544fdc3d4ffeea08b8c9309
Sam <samuel.waters@gmail.com>
2023-05-21 20:00:50 -0600

    chore: added font

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ clock = pygame.time.Clock()
 # test_font = pygame.font.Font(None, 50)
 test_font = pygame.font.Font('font/Pacifico.ttf', 50)
 
+# characters
+evil_snail = pygame.image.load('graphics/characters/png-transparent-snail-character-the-lich-slug-orb-snail-animals-hand-caracol.png')
+
 # test text surface for menu
-welcome_message = test_font.render("Welcome to My Game", False, "Green")
+welcome_message = test_font.render("Welcome to My Game", False, "Black")
 
 background_surface = pygame.image.load('graphics/bg/URy5F.png')
 background_surface = pygame.transform.scale(background_surface, (800, 400))
@@ -24,6 +27,7 @@ while True:
 
     # background
     screen.blit(background_surface, (0, 0))
+    screen.blit(evil_snail, (300, 300))
     screen.blit(welcome_message, (200, 100))
     # x axis, y axis, 0, 0 is the top left corner
     pygame.display.update()
