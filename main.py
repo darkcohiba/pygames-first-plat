6cbe7a06e32c2a5bdfedc27c8f9c91bd80150268
Sam <samuel.waters@gmail.com>
2023-05-21 19:51:30 -0600

    chore: added font

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 # set our font to default with a size of 50
-test_font = pygame.font.Font(None, 50)
+# No font added
+# test_font = pygame.font.Font(None, 50)
+test_font = pygame.font.Font('font/Pacifico.ttf', 50)
 
 # test text surface for menu
 welcome_message = test_font.render("Welcome to My Game", False, "Green")
-
-
-
 
 background_surface = pygame.image.load('graphics/bg/URy5F.png')
 background_surface = pygame.transform.scale(background_surface, (800, 400))
@@ -29,4 +28,3 @@ while True:
     # x axis, y axis, 0, 0 is the top left corner
     pygame.display.update()
     clock.tick(60)
-
